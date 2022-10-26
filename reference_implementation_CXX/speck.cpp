9d72355932efa6d8a9c762edc5025c19a85f0650
Florian Frank <Florian.Frank@uni-passau.de>
2022-10-26 09:59:42 +0200

    Add comments and cleanup code in speck implementation

--- reference_implementation_CXX/speck.cpp
+++ reference_implementation_CXX/speck.cpp
@@ -1,10 +1,18 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <inttypes.h>
+/**
+ * @brief Reference implementation of of SPECK 64/128.
+ * @copyright Virginia Tech & University of Passau
+ */
 
+#include <cinttypes> // PRIu64
+#include <iostream>
+
+// Rotate right
 #define ROR(x, r) ((x >> r) | (x << (64 - r)))
+// Rotate left
 #define ROL(x, r) ((x << r) | (x >> (64 - r)))
+// Round function
 #define R(x, y, k) (x = ROR(x, 8), x += y, x ^= k, y = ROL(y, 3), y ^= x)
+
 #define ROUNDS 32
 void encrypt(uint64_t ct[2], uint64_t const pt[2], uint64_t const K[2]);
 
@@ -12,23 +20,27 @@ void encrypt(uint64_t ct[2], uint64_t const pt[2], uint64_t const K[2]);
 int main() {
     // Write C code here
     uint64_t cyphertext[2] = {0, 0};
-    uint64_t const plaintext[2] = {0100011100101101010010110110000101010000011001000101001101100111, 1100111100010110100010001011001111111011110001000000110000010011};
-    uint64_t const key[2] = {0111010100110111011110000010000101000001001001010100010000101010, 0100011100101101010010110110000101010000011001000101001101100111};
+
+    uint64_t const plaintext[2] = {0100011100101101010010110110000101010000011001000101001101100111,
+                                   1100111100010110100010001011001111111011110001000000110000010011};
+    uint64_t const key[2] = {0111010100110111011110000010000101000001001001010100010000101010,
+                             0100011100101101010010110110000101010000011001000101001101100111};
     encrypt(cyphertext, plaintext, key);
     printf("%" PRIu64 "\n", cyphertext[0]);
     printf("%" PRIu64 "\n", cyphertext[1]);
     return 0;
 }
 
-void encrypt(uint64_t ct[2], uint64_t const pt[2], uint64_t const K[2]) {
-   uint64_t y = pt[0], x = pt[1], b = K[0], a = K[1];
+void encrypt(uint64_t cyptherText[2], uint64_t const plainText[2], uint64_t const key[2]) {
+   uint64_t p0 = plainText[0], p1 = plainText[1], k0 = key[0], k1 = key[1];
 
-   R(x, y, b);
+   R(p1, p0, k0);
+
    for (int i = 0; i < ROUNDS - 1; i++) {
-      R(a, b, i);
-      R(x, y, b);
+      R(k1, k0, i);
+      R(p1, p0, k0);
    }
 
-   ct[0] = y;
-   ct[1] = x;
+    cyptherText[0] = p0;
+    cyptherText[1] = p1;
 }
