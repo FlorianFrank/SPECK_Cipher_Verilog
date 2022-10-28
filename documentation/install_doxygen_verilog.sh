4fd0f212916a356c965e0bc4a9ae41c275a57de8
Florian Frank <Florian.Frank@uni-passau.de>
2022-10-28 17:53:10 +0200

    Add ls to build script for test purposes

--- documentation/install_doxygen_verilog.sh
+++ documentation/install_doxygen_verilog.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-git clone git@github.com:avelure/doxygen-verilog.git
+
+#git clone git@github.com:avelure/doxygen-verilog.git
+ls -al
 mkdir -p build_doxygen
 pushd build_doxygen
 cmake ../doxygen-verilog
