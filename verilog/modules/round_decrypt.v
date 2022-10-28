1f736cb54e057aad3fcd78ef52136d55bcdd18bc
Florian Frank <Florian.Frank@uni-passau.de>
2022-10-28 14:25:50 +0200

    Improve round decrypt function, restructure code

--- verilog/modules/round_decrypt.v
+++ verilog/modules/round_decrypt.v
@@ -1,78 +1,61 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: University of Passau
-// Engineer: Florian Frank
-// 
-// Create Date:    18:32:00 04/02/2022 
-// Design Name: 
-// Module Name:    round 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: Implementation of a SPECK round function.
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
+`include "general_functions.v"
+`include "../defines/round_function_defines.vh"
+
+//% \addtogroup round_function Round Function
+//% @{
 module round_decrypt(
+		//% Input clock running the state machine
 		input clk,
+
+		//% Subkey k1 which is XOR'ed with pt0
+		input [`BLOCK_SIZE-1:0] subkey,
+
+		//% input cipher text splitted into c0 and c1
+		input [`KEY_SIZE-1:0] ciphertext,
+		//% output plaintext of the current round
+		output reg[`KEY_SIZE-1:0] plaintext,
+
+		//% Toggle the signal to 1 to start the round decrypt execution. (Must be reset until the cipher finishes)
 		input signal_start,
-		input [63:0] subkey,
-		input [127:0] ciphertext,
-		output reg[127:0] plaintext,
+		//% Toggle high signal when the execution is finished and the resulting plaintext are can be read.
 		output reg finished,
+
+		//% State only for debug purposes, don't need to be connected
 		output [3:0]state_response
     );
-	 
-	 localparam shiftwidth_p0 = 8;
-	 localparam shiftwidth_p1 = 3;
-	 
-	 
-	 
-	 /** Different states of the state machine. **/
-	 localparam maxState = 6;
-	
-	 parameter WAIT_FOR_START = 0;
-	 parameter ASSIGNMENT = 1;
-	 parameter XOR_P0_P1 = 2;
-	 parameter XOR_SUBKEY_SHIFT_P0 = 3;
-	 parameter ADDITION = 4;
-	 parameter SHIFT = 5;
-	 parameter RESULT_ASSIGNMENT = 6;
-	 
+
+	//% Counter of the state machine
 	 reg [4:0] state = 0;
 	 
-	 reg [63:0] p0;
-	 reg [63:0] p1;
+	 reg [`BLOCK_SIZE-1:0] c0;
+	 reg [`BLOCK_SIZE-1:0] c1;
 	 
 	 task inc_counter;
 		begin
-			if(state < maxState)
+			if(state < `MAX_STATE)
 				state = state + 1;
 			else
 				state = 0;
 		end
-	 endtask;
+	 endtask
 	 
-	 function automatic [63:0] shift_right_reverse;
-		 input [63:0] in;
+	 function automatic [`BLOCK_SIZE-1:0] shift_right_reverse;
+		 input [`BLOCK_SIZE-1:0] in;
 		 input [3:0] shiftwidth;
 		 begin
-			shift_right_reverse = (in <<< shiftwidth) | (in >>> (64 - shiftwidth));
+			shift_right_reverse = (in <<< shiftwidth) | (in >>> (`BLOCK_SIZE - shiftwidth));
 		 end
-	 endfunction;
+	 endfunction
 	 
-	 function automatic [63:0] shift_left_reverse;
-		 input [63:0] in;
+	 function automatic [`BLOCK_SIZE-1:0] shift_left_reverse;
+		 input [`BLOCK_SIZE-1:0] in;
 		 input [3:0] shiftwidth;
 		 begin
-			shift_left_reverse = (in >>> shiftwidth) | (in <<< (64 - shiftwidth));
+			shift_left_reverse = (in >>> shiftwidth) | (in <<< (`BLOCK_SIZE - shiftwidth));
 		 end
-	 endfunction;
+	 endfunction
 	 
 	 initial begin
 		state <= 0;
@@ -81,49 +64,51 @@ module round_decrypt(
 	 
 	 
 	 always @ (posedge clk) begin
-		case(state)
-			WAIT_FOR_START: begin
+
+		 case(state)
+
+			`WAIT_FOR_START_DECRYPT: begin
 				if(signal_start == 1)
 					inc_counter();
 				finished <= 0;
 			end
-			
-			ASSIGNMENT: begin
-				p0 <= ciphertext[63:0];
-				p1 <= ciphertext[127:64];
+
+			`ASSIGNMENT_DECRYPT: begin
+				c0 <= ciphertext[`BLOCK_SIZE-1:0];
+				c1 <= ciphertext[`KEY_SIZE-1:`BLOCK_SIZE];
 				inc_counter();
 			end
-			
-			XOR_P0_P1: begin
-				p1 <= p0 ^ p1;
+
+			`XOR_C0_C1_DECRYPT: begin
+				c1 <= c0 ^ p1;
 				inc_counter();
 			end
-			
-			XOR_SUBKEY_SHIFT_P0: begin
-				p1 <= shift_left_reverse(p1, 3);
-				p0 <= p0 ^ subkey;
+
+			`XOR_SUBKEY_SHIFT_C0_DECRYPT: begin
+				c1 <= shift_left_reverse(c1, `SHIFT_WIDTH_P1);
+				c0 <= c0 ^ subkey;
 				inc_counter();
 			end
-			
-			ADDITION: begin
-				p0 <= p0 - p1;
+
+			`ADDITION_DECRYPT: begin
+				c0 <= c0 - c1;
 				inc_counter();
 			end
-			SHIFT: begin
-				p0 <= shift_right_reverse(p0, 8);
+
+			`SHIFT_DECRYPT: begin
+				c0 <= shift_right_reverse(p0, `SHIFT_WIDTH_P0);
 				inc_counter();
 			end
-			RESULT_ASSIGNMENT: begin
-				plaintext[63:0] <= p0;
-				plaintext[127:64] <= p1;
+
+			`RESULT_ASSIGNMENT_DECRYPT: begin
+				plaintext[`BLOCK_SIZE-1:0] <= c0;
+				plaintext[`KEY_SIZE-1:`BLOCK_SIZE] <= c1;
 				finished <= 1;
 				inc_counter();
 			end
-		endcase;
+		endcase
 		
 	 end
-	 
-
 	assign state_response = state;
-
 endmodule
+//% @}
