7ae21a5625d18de6ee6b29e0ab38bc9d19ba4a4f
FlorianFrank <FlorianFrank@users.noreply.github.com>
2022-10-28 16:32:57 +0000

    Deploying to gh-pages from @ FlorianFrank/SPECK_Cipher_Verilog@038dca5f4c8d6d1ef1f2bd98c48c76e6584476d1 🚀

--- modules.js
+++ modules.js
@@ -1,6 +1,6 @@
 var modules =
 [
     [ "Key Schedule", "df/d7f/group__key__schedule.html", "df/d7f/group__key__schedule" ],
-    [ "Round Function", "d4/dc7/group___p_xB9_xA1_u.html", null ],
+    [ "Round Function", "db/d97/group___x80_xBEh"_v.html", null ],
     [ "Test Benches", "d7/d1b/group__test__bench.html", "d7/d1b/group__test__bench" ]
 ];
